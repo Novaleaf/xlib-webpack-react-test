185dc611edf8854d1305ee135c97e294c60b5f8a
Jason <JasonS@Novaleaf.com>
2016-08-27 07:51:40 -0700

    initial checkin of functional test project

---
+++ README.md
@@ -0,0 +1,6 @@
+# react-webpack-test
+
+
+testground, mostly following instructions from http://www.typescriptlang.org/docs/handbook/react-&-webpack.html but using ts2 and types (not typings)
+
+todo:  vendor bundles, hot loading/auto build
---
+++ src/components/hello.tsx
@@ -0,0 +1,12 @@
+﻿import * as React from "react";
+
+export interface HelloProps {
+    compiler: string;
+    framework: string;
+}
+
+export class Hello extends React.Component<HelloProps, {}>{
+    render() {
+        return <h1>Hello this is {this.props.compiler} and {this.props.framework}!</h1>;
+    }
+}
